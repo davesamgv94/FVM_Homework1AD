9d4d100274662d8999826d6431775ad49fe4ed2b
David Samuel Garcia Vicencio <davesamgv94@gmail.com>
2018-04-06 22:17:23 -0500

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 21:08:02 2018
+
+@author: saiby
+"""
+
+import os 
+
+line = '-' * 70
+print('.'+ line + '.')
+print('|{:^70}|'.format('Tarea 1'))
+print('.'+ line + '.')
+print('|{:^70}|'.format(' Autor: Luis M. De la Cruz, Adaptaciones: Samuel Garcia'))
+print('.'+ line + '.')
+print('|{:^70}|'.format('Menu de Programas'))
+print('.'+ line + '.')
+print('|{:^70}|'.format('1. Ejercicio 4.1'))
+print('.'+ line + '.')
+print('|{:^70}|'.format('2. Ejercicio 4.2'))
+print('.'+ line + '.')
+print('|{:^70}|'.format('3. Ejercicio 4.3'))
+print('.'+ line + '.')
+print('|{:^70}|'.format('4. Ejercicio 5.1'))
+print('.'+ line + '.')
+print('|{:^70}|'.format('5. Ejercicio 3 (Temporal)'))
+print('.'+ line + '.')
+
+programa = int(input("Escribe el numero de programa que deseas ejecutar: "))
+
+
+if programa == 1:
+    os.system('python T_4.1.py')
+elif programa == 2:
+    os.system('python T_4.2.py')
+elif programa == 3:    
+    os.system('python T_4.3.py')
+elif programa == 4:
+    os.system('python T_5.1.py')
+elif programa == 5:
+    os.system('python T_T_3.py')
+else:
+    os.system("exit")
